e0e000ae2234fa80050722a118a1c382bc4fd8c0
Shamil Roshan.N <shamilroshan390@gmail.com>
2025-01-29 19:58:19 +0530

    Configuring production

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,9 +4,13 @@ import dotenv from 'dotenv'
 import cookieParser from 'cookie-parser'
 import cors from 'cors'
 import { app, server } from './lib/socket.js'
+import path from 'path'
 
 import authRouter from './routes/auth.route.js'
 import messageRouter from './routes/message.route.js'
+
+dotenv.config()
+const __diraname = path.resolve()
 
 app.use( express.json({ limit : "10mb" }) )
 app.use( cookieParser() )
@@ -19,7 +23,18 @@ app.use( cors({
 
 app.use('/authentication', authRouter)
 app.use('/message', messageRouter)
-dotenv.config()
+
+if( process.env.NODE_ENV === "production" ) {
+
+    app.use( express.static( path.join( __dirname, "../frontend/dist" ) ) )
+    app.get('*', ( request, response ) => {
+    
+        response.sendFile(path.join(__diraname, "../frontend", "dist", "index.html"))
+    
+    })
+
+
+}
 
 server.listen( 5000, () => {
 
