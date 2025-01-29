dcfe52f1cf760d397a210555cc02994c3c089f02
Shamil Roshan.N <shamilroshan390@gmail.com>
2025-01-29 19:04:25 +0530

    Implementing socket.io

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,11 +3,11 @@ import { connectDB } from './lib/db.js'
 import dotenv from 'dotenv'
 import cookieParser from 'cookie-parser'
 import cors from 'cors'
+import { app, server } from './lib/socket.js'
 
 import authRouter from './routes/auth.route.js'
 import messageRouter from './routes/message.route.js'
 
-const app = express()
 app.use( express.json({ limit : "10mb" }) )
 app.use( cookieParser() )
 app.use( cors({
@@ -21,7 +21,7 @@ app.use('/authentication', authRouter)
 app.use('/message', messageRouter)
 dotenv.config()
 
-app.listen( 5000, () => {
+server.listen( 5000, () => {
 
     console.log('Server is running on 5000')
     connectDB()
